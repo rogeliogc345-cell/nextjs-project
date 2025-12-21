5dae8b5bf71748d4d4f660e33d4a651a6e7bb897
Rogelio <gomez.rogelio@nokia.com>
2025-12-20 22:09:20 -0600

    adding a change

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     
-    <div className="flex flex-col items-center justify-center h-screen bg-amber-100">
-      <h1 className="font-bold text-4xl">Hello World</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-black">
+      <h1 className="font-bold text-4xl text-white">This is my second change</h1>
     </div>
   );
 }
