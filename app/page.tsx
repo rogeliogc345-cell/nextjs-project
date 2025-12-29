5faeae23d72d9a8d4618e75379e5f3bf3acfca4b
Rogelio <rogergc_4@hotmail.com>
2025-12-29 15:07:35 -0600

    change 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
+import CardExample from "@/components/CardExample";
 import Image from "next/image";
 
 export default function Home() {
   return (
     
-    <div className="flex flex-col items-center justify-center h-screen bg-amber-300 p-2.5 gap-3">
+    <div className="flex flex-col items-center justify-center  bg-amber-300 p-2.5 gap-3">
       <h1 className="font-bold text-4xl text-white">New page and adding changes</h1>
       <p className="text-justify  text-blue">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, voluptate! Sapiente quisquam, distinctio commodi minima nam earum quae minus explicabo. Dolore laudantium esse ab, aliquid minima vero error beatae adipisci?
       </p>
+      <CardExample />
+      <Image src="/images/logo.png" alt="logo" width={500} height={500} />
     </div>
   );
 }
